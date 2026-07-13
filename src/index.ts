7885d24b075f4982e3fd59657f08744a1471fd66
Nick Stewart <nick.stewart@pathfindermarketing.com.au>
2026-07-13 15:08:16 +0800

    debug: omit ConversionGoalIds entirely, restore space-delimited multi-type string

--- src/index.ts
+++ src/index.ts
@@ -354,8 +354,7 @@ class BingAdsManager {
   async listConversionGoals(client: ClientConfig): Promise<{ goals: ConversionGoalSummary[]; warnings: GoalWarning[] }> {
     const url = `${CAMPAIGN_MGMT_BASE}/ConversionGoals/QueryByIds`;
     const body = {
-      ConversionGoalIds: [],
-      ConversionGoalTypes: "Url",
+      ConversionGoalTypes: "AppDownload AppInstall Duration Event InStoreTransaction OfflineConversion PagesViewedPerVisit Url",
     };
     const response = await this.apiCall(url, body, client, "listConversionGoals");
     const raw = extractConversionGoalsArray(response);
